e10824293997a0bf94087490d344c6dff452aae8
ye.yin <ye.yin@24tidy.com>
2016-09-25 13:27:58 +0800

    去除文件

--- config/params-local.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'postHost' => 'http://localhost/PlantWebService/web',
-    'updateAppVersion' => '1.0.1',
-    'updateAppUrl' => 'http://expressvideo.oss-cn-hangzhou.aliyuncs.com/appupdate/H583BF980.wgt',
-];

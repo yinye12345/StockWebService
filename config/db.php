2c0899b08d9edc692c954e57c109f6ecc23a9269
ye.yin <ye.yin@24tidy.com>
2016-09-26 11:05:27 +0800

    提交文件“

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=192.168.40.157;dbname=tidy',
+        'username' => 'tidy',
+        'password' => '24tidy.com',
+        'charset' => 'utf8'
+    ],
+];

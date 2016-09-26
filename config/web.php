50029de91cf4a3f020e165546f128015d8e3db8c
ye.yin <ye.yin@24tidy.com>
2016-09-26 10:58:00 +0800

    提交配置

--- config/web.php
+++ config/web.php
@@ -1,7 +1,7 @@
 <?php
 
-$params = require(__DIR__ . '/params-local.php');
-$dbConfig = require(__DIR__ . '/db-local.php');
+$params = require(__DIR__ . '/params.php');
+$dbConfig = require(__DIR__ . '/db.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
